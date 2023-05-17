e5e7ca75abef9474428a2ae835de01e454746257
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2023-05-16 22:38:29 -0300

    Iniciando o formulário

--- src/components/financas/index.tsx
+++ src/components/financas/index.tsx
@@ -5,14 +5,52 @@ import Conteudo from "../templates/Conteudo";
 import Pagina from "../templates/Pagina";
 import Lista from "./Lista";
 import transacoesFalsas from "@/data/constants/transacoesFalsas";
+import Formulario from "./Formulario";
+import NaoEncontrado from "../templates/NaoEncotrado";
+import Id from "@/logic/core/comum/Id";
 
 export default function Financas() {
     const [transacoes, setTransacoes] = useState<Transacao[]>(transacoesFalsas);
+    const [transacao, setTransacao] = useState<Transacao | null>(null);
+
+    function salvar(transacao: Transacao) {
+        const outrasTransacoes = transacoes.filter(t => t.id !== transacao.id);
+        setTransacoes([...outrasTransacoes, {
+            ...transacao,
+            id: transacao.id ?? Id.novo()
+        }]);
+        setTransacao(null);
+    }
+
+    function excluir(transacao: Transacao) {
+        const outrasTransacoes = transacoes.filter(t => t.id !== transacao.id);
+        setTransacoes(outrasTransacoes);
+        setTransacao(null);
+        
+    }
+
     return (
         <Pagina>
             <Cabecalho />
-            <Conteudo>
-                <Lista transacoes={transacoes} />
+            <Conteudo className="gap-5">
+                {transacao ? (
+                    <Formulario 
+                        transacao={transacao} 
+                        salvar={salvar}
+                        excluir={excluir}
+                        cancelar={() => setTransacao(null)}
+                    />
+                ) : transacoes.length ? (
+                    <Lista 
+                        transacoes={transacoes} 
+                        selecionarTransacao={setTransacao} 
+                    />
+
+                ) : (
+                    <NaoEncontrado>
+                        Nenhuma transação encontrada
+                    </NaoEncontrado>
+                )}
             </Conteudo>
             
         </Pagina>
